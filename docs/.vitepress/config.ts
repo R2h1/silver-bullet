6a135b940b8a8b1b3fbc760ad7f7c546c3226a6e
R2h1 <1461694018@qq.com>
2023-11-29 13:02:37 +0800

    feat: 支持本地搜索

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -8,6 +8,30 @@ export default defineConfig({
   lastUpdated: true,
   cleanUrls: true,
   themeConfig: {
+    search: {
+      provider: 'local',
+      options: {
+        locales: {
+          root: { //这里是个大坑，zh是不生效的，改为root即可
+            translations: {
+              button: {
+                buttonText: '搜索文档',
+                buttonAriaLabel: '搜索文档'
+              },
+              modal: {
+                noResultsText: '无法找到相关结果',
+                resetButtonTitle: '清除查询条件',
+                footer: {
+                  selectText: '选择',
+                  navigateText: '切换',
+                  closeText: '关闭'
+                }
+              }
+            }
+          }
+        }
+      }
+    },
     outline: [2, 3],
     nav: [
       { text: '计算机基础', link: '/basics/data-structures-algorithms' }
